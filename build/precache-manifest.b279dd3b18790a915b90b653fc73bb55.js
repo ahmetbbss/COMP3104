d8c4da3b6790702a09f74f6ded8a3e84c9344834
Ahmet Buyukbas <ahmetbbss@users.noreply.github.com>
2022-10-23 22:12:16 -0400

    React project code added

---
+++ build/precache-manifest.b279dd3b18790a915b90b653fc73bb55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c9b80ecda8183ba046b",
+    "url": "/ahmetbbss/COMP3104/static/css/main.32368ef0.chunk.css"
+  },
+  {
+    "revision": "4c9b80ecda8183ba046b",
+    "url": "/ahmetbbss/COMP3104/static/js/main.4c9b80ec.chunk.js"
+  },
+  {
+    "revision": "7c3314309c754720dd81",
+    "url": "/ahmetbbss/COMP3104/static/js/runtime~main.7c331430.js"
+  },
+  {
+    "revision": "b1773f8afd9fb6a0106e",
+    "url": "/ahmetbbss/COMP3104/static/js/2.b1773f8a.chunk.js"
+  },
+  {
+    "revision": "721e0726f5158ad1736bc970d6677cae",
+    "url": "/ahmetbbss/COMP3104/index.html"
+  }
+];
